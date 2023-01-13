bfb428c9877747deaf69afe765d27b5be702c8c9
Marcos <marcosldlf@gmail.com>
2023-01-13 01:34:54 +0100

    Mitad de trabajo

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,13 @@
 
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+l
+<app/src/main/java/com/example/mainmenu/SettingsActivity.java,e\b\ebaa3fe05d52308107d108489c8213600881c0d6
+h
+8app/src/main/java/com/example/mainmenu/GameActivity.java,1\e\1ef342c81a83020517ebfbc0ace0436134357b09
+h
+8app/src/main/java/com/example/mainmenu/HelpActivity.java,8\6\8628591e9e107796d39884fdaf718d6a10344828
+j
+:app/src/main/java/com/example/mainmenu/ScoresActivity.java,8\a\8a5502781a5ccb601cdeb6b2196d415c0b1dcf39
 h
 8app/src/main/java/com/example/mainmenu/MainActivity.java,c\9\c95d9ed822ec2e3f09fa5e3abf3997e4099cd7db
--- app/src/main/java/com/example/mainmenu/MainActivity.java
+++ app/src/main/java/com/example/mainmenu/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.mainmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,40 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.menu_item, items);
 
         listViewItems.setAdapter(arrayAdapter);
+
+
+
+        // https://stackoverflow.com/questions/22422196/android-listview-listselector-not-working
+
+        listViewItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+
+
+                TextView textView = (TextView) itemClicked;
+                String strText = textView.getText().toString();
+
+                if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_play))) {
+                    startActivity(new Intent(MainActivity.this, GameActivity.class));
+
+                } else if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_help))) {
+                    startActivity(new Intent(MainActivity.this, HelpActivity.class));
+
+                } else if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_settings))) {
+                    startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+
+                } else if (strText.equalsIgnoreCase(getResources().getString(R.string.menu_item_scores))) {
+                    startActivity(new Intent(MainActivity.this, ScoresActivity.class));
+
+                }
+
+
+            }
+
+
+        });
 
 
     }
